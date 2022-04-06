f350d25f0fc308701421a8aa61e09c9d6f4eb681
Turgutaliev <98862640+Turgutaliev@users.noreply.github.com>
2022-04-06 19:45:13 -0400

    created the loop in master

--- src/day6/Problem.java
+++ src/day6/Problem.java
@@ -4,8 +4,8 @@ public class Problem {
     public static void main(String[] args) {
 
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+        for (int j = 0; j < 5; j++) {
+            System.out.println(j);
 
 
         }
